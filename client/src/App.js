b97b6cb00a055806134e603b5d3e6faeb18321b6
mohamad <msiysinyuy@gmail.com>
2021-09-29 15:24:32 +0100

    updateing the master branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,26 @@
+import {BrowserRouter as Router,Switch,Route } from 'react-router-dom'
 
-import './app.css'
-import Landing from "./pages/landingpage/Landing"
-import AdminHome from "./pages/home/Home"
-import UserList from "./pages/userList/UserList"
-import { BrowserRouter as Router, Switch, Route}from "react-router-dom"
-import User from "./pages/user/User"
-import NewUser from "./pages/newUser/NewUser"
-import ProductList from "./pages/productList/ProductList"
-import Product from "./pages/product/Product"
-import NewProduct from "./pages/newProduct/NewProduct"
+//Routing
+import PrivateRoute from "./components/routing/PrivateRoute"
 
-function App() {
+// Screens
+import PrivateScreen from "./components/screens/PrivateScreen"
+import LoginScreen from "./components/screens/LoginScreen"
+import RegisterScreen from "./components/screens/RegisterScreen"
+import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen"
+import ResetPasswordScreen from "./components/screens/ResetPasswordScreen"
+const App=()=> {
   return (
     <Router>
-
-
-
+      <div className='App'>
         <Switch>
-            <Route exact path="/">   <Landing />   </Route>
-            <Route exact path="/admin">   <AdminHome />   </Route>
-            <Route exact path="/users">   <UserList />  </Route>
-            <Route exact path="/user/:userId">   <User />    </Route>
-            <Route exact path="/newUser">    <NewUser />  </Route>
-
-            <Route exact path="/products">  <ProductList />   </Route>
-            <Route exact path="/product/:productId">    <Product /> </Route>
-            <Route exact path="/newproduct">  <NewProduct />   </Route>
-
+            <PrivateRoute exact path="/" component={PrivateScreen}/>
+            <Route exact path="/login" component={LoginScreen}/>
+            <Route exact path="/register" component={RegisterScreen}/>
+            <Route exact path="/forgotpassword" component={ForgotPasswordScreen}/>
+            <Route exact path="/passwordreset/:resetToken" component={ResetPasswordScreen}/>
         </Switch>
-
-    
+      </div>
     </Router>
   );
 }
--- client/src/pages/landingpage/Landing.jsx
+++
@@ -1,9 +0,0 @@
-import './landing.css'
-
-export default function Home(){
-  return(
-    <div className="home">
-    Landing page ...
-    </div>
-  )
-}
